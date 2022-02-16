e515bcc60244926aad4ddf977cc770ba643f78f5
Shadab Mehmood <92544133+shadabmehmood@users.noreply.github.com>
2022-02-16 17:59:08 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,9 +2,9 @@
 //help function will list all the ways by which you can run the command sof this projects
 function helpFn() {
     console.log(`List of Command->
-                                     1)Tree - node Fo.js tree <dirPath>
-                                     2)Organize - node Fo.js <dirPath>
-                                     3)Help - node Fo.js help`);
+                                     1)Tree - node main.js tree <dirPath>
+                                     2)Organize - node main.js <dirPath>
+                                     3)Help - node main.js help`);
   }
 
 
